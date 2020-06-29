f30b13fba2dc04f54cc444e4b1a7c08a35bf4883
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2020-06-29 21:58:59 +0300

    Вынесение подсчета за отдельную пьесу в функцию

--- main.js
+++ main.js
@@ -8,25 +8,8 @@ function statement(invoice) {
             minimumFractionDigits: 2
         }).format;
     for (let perf of invoice.performance) {
-        
-        let thisAmount = 0;
-        switch (perf.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`неизвестный тип: ${perf.type}`);
-        }
+        let thisAmount = calculatePerformancePrice(perf.audience, perf.type);
+
         // Добавление бонусов
         volumeCredits += Math.max(perf.audience - 30, 0);
         // Дополнительный бонус за каждые 10 комедий
@@ -41,4 +24,27 @@ function statement(invoice) {
     result += `Итого с вас ${format(totalAmount)}\n`;
     result += `Вы заработали ${volumeCredits} бонусов\n`;
     return result;
+}
+
+function calculatePerformancePrice(audience, type) {
+    let thisAmount = 0;
+    switch (type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (audience > 30) {
+                thisAmount += 1000 * (audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (audience > 20) {
+                thisAmount += 10000 + 500 * (audience - 20);
+            }
+            thisAmount += 300 * audience;
+            break;
+        default:
+            throw new Error(`неизвестный тип: ${type}`);
+
+    }
+    return thisAmount;
 }
